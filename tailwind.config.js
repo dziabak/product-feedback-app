b8402e704ea0f7cacfe1099c78300d1971648b32
dziabak <86873665+dziabak@users.noreply.github.com>
2024-01-27 22:59:15 +0100

    Added styles configuration in 'tailwind.config.js' Imported fonts in 'index.css' Added 'assets' folder in 'public' directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: { main: ["Jost", "sans-serif"] },
+		extend: {
+			colors: {
+				"c-light-blue": "#4661E6",
+				"c-dark-blue": "#3A4374",
+				"c-gray": "#F2F4FF",
+				"c-light-gray": "#F7F8FD",
+				"c-dark-gray": "#647196",
+				"c-orange": "#F49F85",
+				"c-aqua": "#62BCFA",
+				"c-magenta": "#AD1FEA",
+			},
+			container: {
+				center: true,
+				padding: {
+					// DEFAULT: "1rem",
+					// sm: "4rem",
+					lg: "4rem",
+					xl: "24rem",
+					// "2xl": "6rem",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
