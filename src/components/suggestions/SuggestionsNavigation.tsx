a973a109702c91fc3f89f3e9d498fcc9a7feca28
dziabak <86873665+dziabak@users.noreply.github.com>
2024-02-04 22:35:15 +0100

    Added 'SuggestionDarkMode' toggle component

--- src/components/suggestions/SuggestionsNavigation.tsx
+++ src/components/suggestions/SuggestionsNavigation.tsx
@@ -2,12 +2,14 @@
 import SuggestionsHeader from "./SuggestionsHeader";
 import SuggestionsCategoryFilter from "./SuggestionsCategoryFilter";
 import SuggestionsRoadmapView from "./SuggestionsRoadmapView";
+import SuggestionsDarkMode from "./SuggestionsDarkMode";
 
 const SuggestionsNavigation = () => {
 	return (
 		<div>
-			<div className="space-y-4 md:space-y-0 md:grid md:grid-cols-3 md:gap-4 lg:grid-rows-3 lg:grid-cols-1">
+			<div className="space-y-4 md:space-y-0 md:grid md:grid-cols-3 md:gap-4 lg:block lg:space-y-4">
 				<SuggestionsHeader />
+				<SuggestionsDarkMode />
 				<SuggestionsCategoryFilter />
 				<SuggestionsRoadmapView />
 			</div>
