06c40c3fec151d10f43259189a2c7016ca23ab44
dziabak <86873665+dziabak@users.noreply.github.com>
2024-02-06 19:29:12 +0100

    Fixed attribute names for svg element in 'FeedbackTile'

--- src/components/feeedback-tile/FeedbackTile.tsx
+++ src/components/feeedback-tile/FeedbackTile.tsx
@@ -38,9 +38,9 @@ const FeedbackTile = ({
 						<path
 							d="M1 6l4-4 4 4"
 							stroke="#4661E6"
-							stroke-width="2"
+							strokeWidth="2"
 							fill="none"
-							fill-rule="evenodd"
+							fillRule="evenodd"
 						/>
 					</svg>
 					<p>{upvotes}</p>
