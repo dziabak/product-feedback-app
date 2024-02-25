b1658aa82fbaf2ca7df84e98ca3db4fcd4d98fac
dziabak <86873665+dziabak@users.noreply.github.com>
2024-02-25 13:20:42 +0100

    Improved focus state for 'GenericButton' and 'LinkButton'

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -18,11 +18,15 @@ const LinkButton = ({
 		<Link
 			to={linkTo}
 			className={clsx(
-				`px-4 py-3 text-sm text-center font-bold whitespace-nowrap transition-colors rounded-[10px] text-white md:px-6`,
-				color === "magenta" && "bg-c-magenta hover:bg-c-magenta-hover",
-				color === "light-blue" && "bg-c-light-blue hover:bg-c-light-blue-hover",
-				color === "dark-blue" && "bg-c-dark-blue hover:bg-c-dark-blue-hover",
-				color === "red" && "bg-c-red hover:bg-c-red-hover",
+				`px-4 py-3 text-sm text-center font-bold whitespace-nowrap transition-colors rounded-[10px] border border-transparent text-white md:px-6`,
+				color === "magenta" &&
+					"bg-c-magenta hover:bg-c-magenta-hover focus:outline-none focus:ring focus:ring-c-magenta",
+				color === "light-blue" &&
+					"bg-c-light-blue hover:bg-c-light-blue-hover focus:outline-none focus:ring focus:ring-c-light-blue",
+				color === "dark-blue" &&
+					"bg-c-dark-blue hover:bg-c-dark-blue-hover focus:outline-none focus:ring focus:ring-c-dark-blue",
+				color === "red" &&
+					"bg-c-red hover:bg-c-red-hover focus:outline-none focus:ring focus:ring-c-red",
 				mobile && "w-full md:w-fit"
 			)}>
 			{text}
