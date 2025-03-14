8e860c053db326c23002f7b43b958dbdbd5bb3a1
Grzegorz <gd.gdabrowski@gmail.com>
2025-03-14 15:38:56 +0100

    Added filtering tests

--- cypress/e2e/filtering.cy.ts
+++ cypress/e2e/filtering.cy.ts
@@ -1,5 +1,31 @@
-describe('filtering feedback', () => {
-    it('should filter feedback by All', () => { }
+/// <reference types="Cypress" />
 
-    )
-})
+describe("filtering feedback", () => {
+	it("should filter feedback by All", () => {
+		cy.visit("/");
+		cy.get('[data-cy="bug-filter-btn"').click();
+		cy.get('[data-cy="all-filter-btn"').click();
+		cy.get('[data-cy="feedback-tile"]').should("have.length", 6);
+	});
+
+	it("should filter feedback by a single category ('bug')", () => {
+		cy.visit("/");
+		cy.get('[data-cy="bug-filter-btn"').click();
+		cy.contains('[data-cy="feedback-tile"]', /bug/i);
+	});
+
+	it("should filter feedback by two categories ('bug' & 'feature')", () => {
+		cy.visit("/");
+		cy.get('[data-cy="bug-filter-btn"').click();
+		cy.get('[data-cy="feature-filter-btn"').click();
+		cy.contains('[data-cy="feedback-tile"]', /bug/i);
+		cy.contains('[data-cy="feedback-tile"]', /feature/i);
+	});
+
+	it("should display empty page if there are no filtered feedback items", () => {
+		cy.visit("/");
+		cy.get('[data-cy="ui-filter-btn"').click();
+		cy.get('[data-cy="feedback-tile"]').should("have.length", 0);
+		cy.get('[data-cy="no-feedback-element"]');
+	});
+});
